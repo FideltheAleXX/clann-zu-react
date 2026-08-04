aef10362837f384c45ecf378ab0ee5ce0119a500
theAleXX <th3alexx@gmail.com>
2026-08-04 17:41:09 +0200

    add robots.txt, sitemap and react-helmet-async

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,19 @@
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import Navigation from '../components/navigation/Navigation';
+import SEO from '../components/seo/SEO';
 import mainFoto from '../assets/clann-zu.jpg';
 import styles from './MainPage.module.css';
 
 const MainPage = () => {
   return (
     <>
+      <SEO
+        title="Clann Zú fan-site"
+        description="Clann Zú fan-site for your enjoy."
+        name="Clann Zú"
+        type="article"
+      />
       <Header />
       <Navigation />
       <main className={styles.mainSection}>
